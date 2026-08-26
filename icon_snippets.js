f46b29c4d198bb8fb3628c0a9c8aafe9b35c92ea
mft0002smasra-coder <mft0002.smasra@gmail.com>
2026-08-26 12:06:51 +0800

    LP updates

---
+++ icon_snippets.js
@@ -0,0 +1,7 @@
+// Tambahkan clipboard dan back ke dalam ICONS di app.js:
+// const ICONS = {
+//   ...
+//   clipboard: '<path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"/><rect x="8" y="2" width="8" height="4" rx="1" ry="1"/>',
+//   back: '<path d="M19 12H5M12 19l-7-7 7-7"/>',
+//   ...
+// };
